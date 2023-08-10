228a4986a147ec554bf2eab78c1db26fbbaaedb3
St-Daniel-H <danihujairy66@gmail.com>
2023-08-10 13:56:20 +0300

    added events detail

--- calenderAPI/Pages/MeetingRooms/src/Home/Calender/Calender.jsx
+++ calenderAPI/Pages/MeetingRooms/src/Home/Calender/Calender.jsx
@@ -1,77 +1,106 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { render } from "react-dom";
 import events from "./events";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-  //import 'react-big-calendar/lib/sass/styles';
-  //import 'react-big-calendar/lib/addons/dragAndDrop/styles';/// Corrected typo: "indes.css" to "index.css"
+//import 'react-big-calendar/lib/sass/styles';
+//import 'react-big-calendar/lib/addons/dragAndDrop/styles';/// Corrected typo: "indes.css" to "index.css"
 import "./Calendar.scss";
+import EventsAnchor from "./EventAnchor";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
 moment.locale("en-GB");
 
 const localizer = momentLocalizer(moment);
 
-
 const eventss = [
-    {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    },
-    {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    },
-    {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    }, {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    }, {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    }, {
-        title: "hey",
-        description: "room321",
-        start: new Date(2023, 7, 14, 10, 0),
-        end: new Date(2023, 7, 14, 12, 0),
-    }
-]
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+  {
+    title: "hey",
+    description: "room321",
+    start: new Date(2023, 7, 14, 10, 0),
+    end: new Date(2023, 7, 14, 12, 0),
+  },
+];
 function CalendarView() {
-    const minTime = moment().set("hour", 8).set("minute", 0).toDate();
-    const maxTime = moment().set("hour", 20).set("minute", 0).toDate();
+  //events drawer
+  const [EventAncorState, setEventAncorState] = useState({
+    right: false,
+  });
+  const anchor = "right";
 
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-    return (
-        <div style={{ height: 700 }}>
+    return setEventAncorState({ [anchor]: open });
+  };
+  //
+  const minTime = moment().set("hour", 8).set("minute", 0).toDate();
+  const maxTime = moment().set("hour", 20).set("minute", 0).toDate();
 
-            <Calendar
-                localizer={localizer}
-                events={eventss}
-                startAccessor="start"
-                endAccessor="end"
-                step={30}
-                views={[Views.MONTH, Views.WEEK, Views.DAY,Views.AGENDA]} // Use allViews here
-                defaultDate={new Date(2023, 3, 1)}
-                tooltipAccessor="description" 
-                min={minTime}
-                max={maxTime}
-                onSelectEvent={(event) => {
-                    console.log(event);
-                }}
-            />
-        </div>
-    );
+  return (
+    <div style={{ height: 700 }}>
+      <Calendar
+        localizer={localizer}
+        events={eventss}
+        startAccessor="start"
+        endAccessor="end"
+        step={30}
+        views={[Views.MONTH, Views.WEEK, Views.DAY, Views.AGENDA]} // Use allViews here
+        defaultDate={new Date(2023, 3, 1)}
+        tooltipAccessor="description"
+        min={minTime}
+        max={maxTime}
+        onSelectEvent={(event) => {
+          setEventAncorState({ right: true });
+          console.log(EventAncorState);
+        }}
+      />
+
+      <Drawer
+        anchor={anchor}
+        open={EventAncorState[anchor]}
+        onClose={toggleDrawer(anchor, false)}
+      >
+        <EventsAnchor state={EventAncorState} setState={setEventAncorState} />
+      </Drawer>
+    </div>
+  );
 }
 
 export default CalendarView;
--- calenderAPI/Pages/MeetingRooms/src/Home/Calender/EventAnchor.jsx
+++ calenderAPI/Pages/MeetingRooms/src/Home/Calender/EventAnchor.jsx
@@ -1,78 +1,70 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
-export default function TemporaryDrawer() {
-    const [state, setState] = React.useState({
-        right: false,
-    });
+export default function EventsAnchor({ state, setState }) {
+  //   const [state, setState] = React.useState({
+  //     right: false,
+  //   });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-        setState({ ...state, [anchor]: open });
-    };
+    setState({ [anchor]: open });
+  };
 
-    const list = (anchor) => (
-        <Box
-            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
-
-    return (
-        <div>
-            {['left', 'right', 'top', 'bottom'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                    <Drawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
-                    >
-                        {list(anchor)}
-                    </Drawer>
-                </React.Fragment>
-            ))}
-        </div>
-    );
+  const list = (anchor) => (
+    <Box
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem>
+          <ListItemText>
+            <h2>Events Detail</h2>
+          </ListItemText>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem id="eventTitle">
+          <h2>Title</h2>{" "}
+        </ListItem>
+        <ListItem id="eventRoom">
+          <ListItemText>
+            <b>Room room123</b>{" "}
+          </ListItemText>
+        </ListItem>
+        <ListItem id="eventTime">
+          <ListItemText>
+            <b>Date: 10/6/2022</b>{" "}
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>From 11:00AM to 2:00pm</ListItemText>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem>
+          <ListItemText>Reserved By Daniel</ListItemText>
+        </ListItem>
+      </List>
+    </Box>
+  );
+  const anchor = "right";
+  return <div>{list(anchor)}</div>;
 }
